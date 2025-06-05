1abc111bfbdb2226f7c599378d08112131154b5a
Abhimanyu <methebestestofall@gmail.com>
2025-06-05 14:06:49 +0000

    fixed calculator

--- html/assets/calc.js
+++ html/assets/calc.js
@@ -1,9 +1,27 @@
 function calculate() {
     const num1 = document.getElementById("num1");
     const num2 = document.getElementById("num2");
+    const operation = document.getElementById("operation").value;
     const resultDiv = document.getElementById("result");
 
-    const result = Number(num1.value) + Number(num2.value);
+    let result;
 
-    resultDiv.innerHTML += "<h2>" + result + "</h2>";
+    switch (operation) {
+        case "addition":
+            result = Number(num1.value) + Number(num2.value);
+            break;
+        case "subtraction":
+            result = Number(num1.value) - Number(num2.value);
+            break;
+        case "multiplication":
+            result = Number(num1.value) * Number(num2.value);
+            break;
+        case "division":
+            result = Number(num1.value) / Number(num2.value);
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    resultDiv.innerHTML = "<h1>Result: " + result + "</h1>";
 }
