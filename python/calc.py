bb47744cf247e085c244d0b3423ffdc1cab8f52b
Abhimanyu <methebestestofall@gmail.com>
2025-06-06 13:40:55 +0000

    tried to get 10/10 pylint just wanted to be happy

---
+++ python/calc.py
@@ -0,0 +1,32 @@
+"""
+Simple calculator: prompts for two numbers and an operation, then prints the result.
+"""
+
+# get the numbers
+num1 = float(input("Choose a number: "))
+num2 = float(input("Choose another number: "))
+result = None # pylint: disable=invalid-name
+
+# get an operation(there is probably a simpler way to do this)
+print("Choose an operation: ")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+operation = int(input("Choose: "))
+
+# calculate the result (again, there is probably a simpler way to do this)
+
+if operation == 1:
+    result = float(num1 + num2)
+elif operation == 2:
+    result = float(num1 - num2)
+elif operation == 3:
+    result = float(num1 * num2)
+elif operation == 4:
+    result = float(num1 / num2)
+else:
+    print("Whatever you picked, it was not an option!")
+
+if result is not None:
+    print("Result:", result)
